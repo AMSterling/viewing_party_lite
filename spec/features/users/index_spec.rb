f5f80becadfc55e20451854420d3ba8f2bbc1540
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-23 16:59:19 -0600

    Username vs First/Last name

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -2,37 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'the landing page' do
   it 'shows a list of all users as links to their dashboard' do
-    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
-    user2 = User.create!(name: 'Maury Povich', email: 'maury@trashtv.com')
-    user3 = User.create!(name: 'Jenny Jones', email: 'jenny@trashtv.com')
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com')
+    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com')
+    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com')
 
     visit root_path
 
     within "#user-#{user1.id}" do
-      expect(page).to have_link('Geraldo Rivera', href: user_path(user1))
+      expect(page).to have_link('Geraldo')
     end
 
     within "#user-#{user2.id}" do
-      expect(page).to have_link('Maury Povich', href: user_path(user2))
+      expect(page).to have_link('Maury')
     end
 
     within "#user-#{user3.id}" do
-      expect(page).to have_link('Jenny Jones', href: user_path(user3))
+      expect(page).to have_link('Jenny')
     end
 
-    click_link('Geraldo Rivera')
-    expect(current_path).to eq user_path(user1)
+    click_link('Geraldo')
+    expect(current_path).to eq("/users/#{user1.id}")
   end
 
-  it 'has a button to create a new user that directs to the register page' do
-    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
-    user2 = User.create!(name: 'Maury Povich', email: 'maury@trashtv.com')
-    user3 = User.create!(name: 'Jenny Jones', email: 'jenny@trashtv.com')
+  it 'has a button to create a new user' do
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com')
+    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com')
+    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com')
 
     visit root_path
 
     expect(page).to have_button('New User')
-    click_button('New User')
-    expect(current_path).to eq register_path
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,43 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'creating a new user' do
   it 'links to a form to create a new user' do
-    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
-    user2 = User.create!(name: 'Maury Povich', email: 'maury@trashtv.com')
-    user3 = User.create!(name: 'Jenny Jones', email: 'jenny@trashtv.com')
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com')
+    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com')
+    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com')
 
     visit root_path
 
     click_button('New User')
     expect(current_path).to eq(register_path)
 
-    within '#create-user' do
-      fill_in 'Name', with: 'Jerry Springer'
-      fill_in 'Email', with: 'jerry@trashtv.com'
-      fill_in 'Password', with: 'guiltypleasure99'
-      click_on 'Create Account'
-    end
+    fill_in 'Name', with: 'Jerry'
+    fill_in 'Email', with: 'jerry@trashtv.com'
+    fill_in 'Password', with: 'guiltypleasure99'
+    click_on 'Create Account'
 
-    expect(current_path).to eq user_path(User.last)
-    expect(page).to have_content("Jerry Springer's Dashboard")
-  end
-
-  it 'should check for uniqueness of email address and be case insensitive' do
-    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
-
-    visit root_path
-
-    click_button('New User')
-    expect(current_path).to eq(register_path)
-
-    within '#create-user' do
-      fill_in 'Name', with: 'Rivera Geraldo'
-      # Test that capitalization will still result in account not being createable
-      fill_in 'Email', with: 'Geraldo@trashtv.com'
-      fill_in 'Password', with: 'password1234'
-      click_on 'Create Account'
-    end
-    
-    expect(current_path).to eq(register_path)
-    expect(page).to have_content("Error: That email is already associated with an account.")
+    expect(page).to have_content("Jerry's Dashboard")
   end
 end
