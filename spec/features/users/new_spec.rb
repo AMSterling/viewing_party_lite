33c8b34ab87a7a51ca2e699a76d1dad81cb36c1a
Blake Saylor <47160926+blakesaylor@users.noreply.github.com>
2022-08-23 19:01:56 -0600

    Refactoring
    Updating to match with main

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -11,11 +11,34 @@ RSpec.describe 'creating a new user' do
     click_button('New User')
     expect(current_path).to eq(register_path)
 
-    fill_in 'Name', with: 'Jerry'
-    fill_in 'Email', with: 'jerry@trashtv.com'
-    fill_in 'Password', with: 'guiltypleasure99'
-    click_on 'Create Account'
+    within '#create-user' do
+      fill_in 'Name', with: 'Jerry'
+      fill_in 'Email', with: 'jerry@trashtv.com'
+      fill_in 'Password', with: 'guiltypleasure99'
+      click_on 'Create Account'
+    end
 
+    expect(current_path).to eq user_path(User.last)
     expect(page).to have_content("Jerry's Dashboard")
+  end
+
+  it 'should check for uniqueness of email address and be case insensitive' do
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com')
+
+    visit root_path
+
+    click_button('New User')
+    expect(current_path).to eq(register_path)
+
+    within '#create-user' do
+      fill_in 'Name', with: 'Rivera'
+      # Test that capitalization will still result in account not being createable
+      fill_in 'Email', with: 'Geraldo@trashtv.com'
+      fill_in 'Password', with: 'password1234'
+      click_on 'Create Account'
+    end
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Error: That email is already associated with an account.")
   end
 end
